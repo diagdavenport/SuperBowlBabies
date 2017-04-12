16c53cca85727c9a47410cbdabc71c1c1b8d576e
diagdavenport <diag.davenport@gmail.com>
2017-04-12 18:57:13 -0400

    Blah

--- Raw/README.txt
+++ Raw/README.txt
@@ -1,2 +1,3 @@
-Births by county and month was accessed 20117.02.16 on https://wonder.cdc.gov/controller/datarequest/D66;jsessionid=5793FAF6D87267DB3E31053D0F4C74A1
+Births by county and month was accessed 2017.02.16 on https://wonder.cdc.gov/controller/datarequest/D66;jsessionid=5793FAF6D87267DB3E31053D0F4C74A1
 Births by county and month with man edits is a copy with the notes field manually removed by Diag
+Economic activity was accessed 2017.04.07 on https://www.bea.gov/national/index.htm#gdp by Diag. Note that the q1 values were parsed out manually by Diag.
--- Scripts/02 Model implementation.R
+++ Scripts/02 Model implementation.R
@@ -1,5 +1,7 @@
 
 
+library(boot)
+
 final.data$Quarter <- ceiling(final.data$Month.Code/3)
 
 final.data <- subset(final.data, select = c(City, Year, Quarter, Births)) 
@@ -28,7 +30,7 @@ for ( place in unique(final.data$City)) {
       
       vector.of.Q1to3.changes[year-2007] <- (sum(subset(final.data, City == place & Group.2 == year & Quarter != 4, select =  Births)) - 
                                             sum(subset(final.data, City == place & Group.2 == year-1 & Quarter != 4, select =  Births)) ) /
-        sum(subset(final.data, City == place & Group.2 == year-1 & Quarter == 1, select =  Births))
+                                            sum(subset(final.data, City == place & Group.2 == year-1 & Quarter != 4, select =  Births))
       
     }
 
@@ -61,3 +63,93 @@ for ( place in unique(final.data$City)) {
 }
 
 dev.off()
+
+### now munge into a regression-ready format 
+
+full.reg.data <- data.frame(changes = double(), participated = logical(), won = logical(), row.num = integer()) # initialize for later
+
+econ.act <- read.csv("Raw/Economic activity.csv", header = T, stringsAsFactors = FALSE)
+econ.act$GDP..2009.dollars. <- as.numeric(econ.act$GDP..2009.dollars.)
+econ.act$row.num <- 1:8
+
+# capture the q4 changes in one vector
+
+for ( place in unique(final.data$City)) { 
+  
+  # Generate the YoY differences by quarter
+  vector.of.Q4.changes <- rep("NA", 8)
+
+  for (year in 2008:2015){
+    
+    vector.of.Q4.changes[year-2007] <- (subset(final.data, City == place & Group.2 == year & Quarter == 4, select =  Births) - 
+                                          subset(final.data, City == place & Group.2 == year-1 & Quarter == 4, select =  Births) ) /
+      subset(final.data, City == place & Group.2 == year-1 & Quarter == 4, select =  Births)
+    
+  }
+  
+  part.vec <- rep(FALSE, 8)
+  w.vec <- rep(FALSE, 8)
+  
+  if (place == "Pittsburgh") {
+    part.vec[c(2009-2007, 2011 - 2007)] <- TRUE
+    w.vec[2009-2007] <- TRUE
+  }
+  else if (place == "Baltimore"){
+    part.vec[c(2013-2007)] <- TRUE
+    w.vec[2013-2007] <- TRUE
+  }
+  else if (place == "NY"){
+    part.vec[c(2008-2007, 2012 - 2007)] <- TRUE
+    w.vec[c(2008-2007, 2012 - 2007)] <- TRUE
+  }
+  else if (place == "Green Bay") {
+    part.vec[c(2011-2007)] <- TRUE
+    w.vec[c(2011-2007)] <- TRUE
+  }
+  else if (place == "Denver") {
+    part.vec[c(2014-2007)] <- TRUE
+  }
+  else if (place == "Seattle") {
+    part.vec[c(2014-2007, 2015 - 2007)] <- TRUE
+    w.vec[c(2014-2007)] <- TRUE
+  }
+  else if (place == "Arizona"){
+    part.vec[c(2009-2007)] <- TRUE
+  }
+  else if (place == "Indianapolis"){
+    part.vec[c(2010-2007)] <- TRUE
+  }
+  else if (place == "New England"){
+    part.vec[c(2008-2007, 2012 - 2007, 2015 - 2007)] <- TRUE
+    w.vec[2015 - 2007] <- TRUE
+  }
+  else if (place == "New Orleans"){
+    part.vec[c(2010-2007)] <- TRUE
+    w.vec[c(2010-2007)] <- TRUE
+  }
+  else if (place == "San Francisco"){ 
+    part.vec[c(2013-2007)] <- TRUE
+  }
+  else BREAK
+  
+  current.reg.data <- data.frame(row.num = 1:8 , changes = unlist(vector.of.Q4.changes) , participated = part.vec, won = w.vec)
+  current.reg.data <- merge(current.reg.data, econ.act, by = "row.num")
+  
+  # finally, append to full data set
+  full.reg.data <- rbind(full.reg.data, current.reg.data)
+  
+  
+}
+
+results.glm1 <- glm(changes ~ participated + won + poly(GDP..2009.dollars. , 1) , data = full.reg.data)
+results.glm3 <- glm(changes ~ participated + won + poly(GDP..2009.dollars. , 3) , data = full.reg.data)
+
+summary(results)
+
+#Sensitivity
+set.seed(1914)
+cv.glm(full.reg.data, glmfit = results.glm3)$delta
+
+#vs.
+
+cv.glm(full.reg.data, glmfit = results.glm1)$delta
